66fb76aa0b9356c036d5027f7ef68666acbfdf9a
ABeeinSpace <planes4u@outlook.com>
2021-06-16 21:41:19 -0400

    Fixing layout issues

---
+++ InsultGenerator-Win/InsultForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace InsultGenerator_Win
+{
+    public partial class InsultView : Form
+    {
+        private Random rng;
+        public ArrayList generatedInsults;
+        public InsultView()
+        {
+            InitializeComponent();
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string insult = InsultGeneratorInator(rng);
+            label1.Text = insult;
+        }
+        
+        // ReSharper disable once CommentTypo
+        // InsultGeneratorInator()
+        // Parameters: Random rng, ArrayList generatedInsult
+        // Returns: string insult
+        // Description: Generates an insult from three ArrayLists of possible values, saves it to the generatedInsults ArrayList so we can reference it later, and then returns the generated insult so that we can assign it to InsultLabel's StringValue later on. Technically I could do that here, but I didn't, so cry more. 
+        // ReSharper disable once IdentifierTypo
+        private string InsultGeneratorInator(Random rng) {
+			
+            // ReSharper disable once StringLiteralTypo
+            string[] firstHalfOfInsultArray = new string[] {"warped", "rougish", "fobbing", "babbling"};
+            string[] middleOfInsultArray = new string[] {"swag-bellied", "sheep-biting", "puke-stockinged", "hag-born"};
+            // ReSharper disable once StringLiteralTypo
+            string[] finalBitOfInsultArray = new string[] {"ruinous-butt!", "tyrant!", "misbegotten-divel!", "coward!"};
+
+            string beginningOfInsult = (string)firstHalfOfInsultArray.GetValue(rng.Next(4));
+            string middleOfInsult = (string)middleOfInsultArray.GetValue(rng.Next(4));
+            string endOfInsult = (string)finalBitOfInsultArray.GetValue(rng.Next(4));
+
+            var insult = $"Thou {beginningOfInsult} {middleOfInsult} {endOfInsult}";
+            generatedInsults.Add(insult);
+            return insult;
+        }
+
+        private void InsultView_Load(object sender, EventArgs e)
+        {
+            rng = new Random();
+            generatedInsults = new ArrayList();
+            string insult = InsultGeneratorInator(rng);
+            label1.Text = insult;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ViewPreviousInsultsForm previousInsultsForm = new ViewPreviousInsultsForm(generatedInsults);
+            previousInsultsForm.Show();
+        }
+    }
+}
---
+++ InsultGenerator-Win/ViewPreviousInsultsForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Windows.Forms;
+
+namespace InsultGenerator_Win
+{
+    public partial class ViewPreviousInsultsForm : Form
+    {
+        private ArrayList generatedInsults;
+        private string generatedInsultsString;
+        
+        public ViewPreviousInsultsForm(ArrayList generatedInsults)
+        {
+            InitializeComponent();
+            this.generatedInsults = generatedInsults;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Save things here.
+            SaveFileDialog saveInsultsDialog = new SaveFileDialog();
+            saveInsultsDialog.Title = "Save Insult List";
+            saveInsultsDialog.Filter = "Text File|*.txt";
+            saveInsultsDialog.ShowDialog();
+            if (saveInsultsDialog.FileName != "")
+            {
+                StreamWriter fs = new StreamWriter(saveInsultsDialog.OpenFile());
+                fs.Write(generatedInsultsString);
+                fs.Close();
+            }
+        
+        }
+
+        private void clearInsultArrayButton_Click(object sender, EventArgs e)
+        {
+            generatedInsults.Clear();
+            this.Close();
+        }
+
+        private void ViewPreviousInsultsForm_Load(object sender, EventArgs e)
+        {
+            generatedInsultsString = "";
+            for (int i = 0; i < generatedInsults.Count; i++)
+            {
+                generatedInsultsString += generatedInsults[i] + "\n\n";
+            }
+
+            insultListLabel.Text = generatedInsultsString;
+        }
+    }
+}
