6b8505ae2eb25e738ef1bd1db4536bed245671f7
Aidan Border <planes4u@outlook.com>
2021-07-11 12:50:30 -0400

    In theory, this commit should enable a built-in updater on Windows. I have not been able to test it yet, so this might just explode it. We shall see.

--- InsultGenerator-Win/Form1.cs
+++ InsultGenerator-Win/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using AutoUpdaterDotNET;
 
 namespace InsultGenerator_Win
 {
@@ -18,7 +19,14 @@ namespace InsultGenerator_Win
 
         private void checkForUpdatesButton_Click(object sender, EventArgs e)
         {
-            System.Diagnostics.Process.Start("explorer.exe", "https://github.com/ABeeinSpace/ShakespeareInsultGenerator/releases/");
+            AutoUpdater.Start("http://rbsoft.org/updates/AutoUpdaterTest.xml");
+            // string message = "You have version " + Application.ProductVersion + ". Pressing OK will open the GitHub releases page for this program so that you can look for any updates. (I know this isn't great, I'm working on making this better)";
+            // string caption = "Checking for Updates";
+            // var result = MessageBox.Show(message, caption, MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            // if (result == DialogResult.OK)
+            // {
+            //     System.Diagnostics.Process.Start("explorer.exe", "https://github.com/ABeeinSpace/ShakespeareInsultGenerator/releases/");
+            }
         }
     }
 }
