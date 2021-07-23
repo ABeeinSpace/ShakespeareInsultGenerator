bf8d711991983cf9c0ad2269f28e1836ec11d384
Aidan Border <planes4u@outlook.com>
2021-07-23 10:18:18 -0400

    Attempting to make auto-updating work

--- InsultGenerator-Win/Form1.cs
+++ InsultGenerator-Win/Form1.cs
@@ -19,14 +19,13 @@ namespace InsultGenerator_Win
 
         private void checkForUpdatesButton_Click(object sender, EventArgs e)
         {
-            AutoUpdater.Start("http://rbsoft.org/updates/AutoUpdaterTest.xml");
+            AutoUpdater.Start("https://github.com/ABeeinSpace/ShakespeareInsultGenerator/blob/master/AutoUpdate.xml");
             // string message = "You have version " + Application.ProductVersion + ". Pressing OK will open the GitHub releases page for this program so that you can look for any updates. (I know this isn't great, I'm working on making this better)";
             // string caption = "Checking for Updates";
             // var result = MessageBox.Show(message, caption, MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
             // if (result == DialogResult.OK)
             // {
             //     System.Diagnostics.Process.Start("explorer.exe", "https://github.com/ABeeinSpace/ShakespeareInsultGenerator/releases/");
-            }
         }
     }
 }
